de425fdc316624eb3fd74c41f2c4798c71cfbcf2
egyfazri <egyfazri23@gmail.com>
2022-02-21 01:18:49 +0700

    feat: create sidebar menu for mobile and desktop

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import 'react-pro-sidebar/dist/css/styles.css';
-import { useState } from 'react';
+import React from 'react';
+import { MobileNav, Sidebar } from './components'
+import Dashboard from './views/dashboard/index'
+import Views from './views/index'
 
 function App() {
-  const [collapsed, setCollapsed] = useState(false)
   return (
-    <div style={{ display: 'flex', flexDirection: 'row'}}>
-      <ProSidebar collapsed={collapsed}>
-        <h1 className='text-center'>Hi Recruiter!</h1>
-        <Menu iconShape="square">
-          <MenuItem >Dashboard</MenuItem>
-          <SubMenu title="Components">
-            <MenuItem>Component 1</MenuItem>
-            <MenuItem>Component 2</MenuItem>
-          </SubMenu>
-        </Menu>
-      </ProSidebar>
-    <div>
-      <h1>Hello</h1>
-    </div>
-    </div>
+      <div>
+        <MobileNav />
+        <div className='flex flex-row h-[100vh] w-full 2xl:container 2xl:mx-auto mt-5 md:mt-0'>
+          <div className="hidden lg:block" >
+            <Sidebar/>
+          </div>
+          <Views />
+        </div>
+      </div>
   );
 }
 
