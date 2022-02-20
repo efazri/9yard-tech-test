0e164353d275742855c7a3068c1b8cc92cce0dc5
egyfazri <egyfazri23@gmail.com>
2022-02-21 02:32:35 +0700

    feat: create ui for active jobs

--- src/views/active-jobs/index.js
+++ src/views/active-jobs/index.js
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { JCard } from '../../components';
+const jobs = [
+    {
+        id: 1,
+        jobTitle: 'Customer Relation Officer',
+        companyName: 'PT. ABC',
+        location: 'Jakarta',
+        exp: '1 Tahun',
+        createAt: '22-01-2022'
+    },
+    {
+        id: 2,
+        jobTitle: 'Sales Engineer',
+        companyName: 'PT. Armada Pura Angkasa',
+        location: 'Medan',
+        exp: '1-2 Tahun',
+        createAt: '22-01-2022'
+    }
+]
 
 function ActiveJobs () {
+    const [selectedJobs, setSelectedJobs] = useState(jobs)
+
+    async function onArchiveHandler(data){
+        const current = await localStorage.getItem('archived');
+        const dataStorage = JSON.parse(current);
+        if (current){
+            let flag = false;
+            dataStorage?.map((el) => {
+                if (el === data.id){
+                    flag = true
+                };
+            })
+
+            if (!flag){
+                dataStorage.push(data?.id)
+                localStorage.setItem('archived', JSON.stringify(dataStorage))
+            }
+
+        } else {
+            const id = [data.id]
+            localStorage.setItem('archived', JSON.stringify(id))
+        }
+        
+    }
+
     return (
-        <h1>Active Jobs Worked!</h1>
+        <div className="w-full p-5 mt-5 lg:mt-0 flex flex-wrap">
+            { selectedJobs?.map((el, index) => {
+                return (
+                    <JCard 
+                        data={el} 
+                        type="active" 
+                        key={index} 
+                        onAction={onArchiveHandler}
+                    />
+                )
+            })}
+        </div>
     )
 };
 
--- src/views/archived-jobs/index.js
+++ src/views/archived-jobs/index.js
@@ -1,22 +1,71 @@
-import React from "react";
-import { Card } from './components';
+import React, { useEffect, useState } from "react";
+import { JCard } from '../../components';
 
 const jobs = [
     {
         id: 1,
-        jobTitle: 'CRO',
+        jobTitle: 'Customer Relation Officer',
         companyName: 'PT. ABC',
         location: 'Jakarta',
         exp: '1 Tahun',
+        createAt: '22-01-2022'
+    },
+    {
+        id: 2,
+        jobTitle: 'Sales Engineer',
+        companyName: 'PT. Armada Pura Angkasa',
+        location: 'Medan',
+        exp: '1-2 Tahun',
         createAt: '22-01-2022'
     }
 ]
 
 function ArchivedJobs () {
+    const [selectedJobs, setSelectedJobs] = useState([])
+
+    useEffect(() => {
+        const storage = localStorage.getItem('archived')
+
+        if (storage){
+            const storageParse = JSON.parse(storage);
+            const newData = []
+            jobs?.map(el => {
+                storageParse?.map(id => {
+                    if (el.id === id){
+                        newData.push(el)
+                    }
+                })
+                setSelectedJobs(newData)
+            })
+        }
+    }, [])
+
+    function onActionHandler (data){
+        const storage = localStorage.getItem('archived')
+
+        if (storage){
+            const storageParse = JSON.parse(storage);
+            const newData = []
+            storageParse.map(id => {
+                if (id != data?.id){
+                    newData.push(id)
+                }
+            })
+            localStorage.setItem('archived', JSON.stringify(newData))
+        }
+    }
+
     return (
-        <div className="w-full p-5">
-            { jobs.map((el, index) => {
-                return <Card data={el} key={index} />
+        <div className="w-full p-5 mt-5 lg:mt-0 flex flex-row">
+            { selectedJobs.map((el, index) => {
+                return (
+                    <JCard 
+                        data={el} 
+                        key={index} 
+                        type="archived"
+                        onAction={onActionHandler}
+                    />
+                )
             })}
         </div>
     )
